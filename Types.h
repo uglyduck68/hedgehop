da5762f77cfa8c9630f904a7b1fe13a9d52492db
uglyduck68 <uglyduck68@gmail.com>
2013-12-16 16:59:40 +0900

    add reactor sample

---
+++ Types.h
@@ -0,0 +1,20 @@
+#pragma	once
+
+namespace X1
+{
+
+	const int	INVALID_HANDLE	= -1;
+	typedef unsigned long	EVENT_MASK;
+	
+#ifdef	defined(_LINUX_)
+
+typedef	int		HANDLE;
+
+#elif	defined(_VXWORKS_)
+
+typedef	int		HANDLE;
+
+#elif	defined(_WINDOWS_)
+
+#endif
+}
