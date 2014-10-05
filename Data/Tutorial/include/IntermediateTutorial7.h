2a9ae73fe978745bdef84f45f78748a96c0145b3
uglyduck68 <uglyduck68@gmail.com>
2014-10-05 18:58:02 +0900

    add tutorial files

---
+++ Data/Tutorial/include/IntermediateTutorial7.h
@@ -0,0 +1,34 @@
+
+#pragma once
+#include "StdAfx.h"
+
+#include "baseapplication.h"
+class IntermediateTutorial7 :
+	public BaseApplication, public Ogre::RenderTargetListener
+
+{
+public:
+	IntermediateTutorial7(void);
+	virtual ~IntermediateTutorial7(void);
+
+protected:
+	// derived from FrameListener via BaseApplication
+    virtual void createScene(void);
+    virtual void createFrameListener(void);
+    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+
+
+	// derived from Ogre::RenderTargetListener
+    virtual void preRenderTargetUpdate(const Ogre::RenderTargetEvent& evt);
+    virtual void postRenderTargetUpdate(const Ogre::RenderTargetEvent& evt);
+
+	// from BaseApplication
+	void setupResources(void);
+
+    Ogre::MovablePlane* mPlane;
+    Ogre::Entity* mPlaneEnt;
+    Ogre::SceneNode* mPlaneNode;
+    Ogre::Rectangle2D* mMiniScreen;
+
+};
+
