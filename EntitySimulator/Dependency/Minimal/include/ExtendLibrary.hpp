806dc6f323aecb49e2d25d8fdfd7379d2bef1eb4
uglyduck68 <uglyduck68@gmail.com>
2015-03-10 12:02:34 +0900

    add some docs

--- EntitySimulator/Dependency/Minimal/include/ExtendLibrary.hpp
+++
@@ -1,8 +0,0 @@
-#include "interface/ICommandInterfaceDDS.h"
-#include "msg/rmOPV_Protocol.hpp"
-#define DOMAIN_ID 2
-ADD_DDSCommand(2,SC_SIM_OWNSHIP);
-ADD_DDSCommand(2,SC_SIM_ENTITY);
-ADD_DDSCommand(2,SC_SIM_ENTITY_STATE);
-ADD_DDSCommand(2,SC_SIM_OWNSHIP_STATE);
-ADD_DDSCommand(2,SC_SIM_ENVIRONMENT);
